bf50f077d184473662222e69d71ec36df05f8ece
William Bautista <BC24003@ues.edu.sv>
2024-09-09 18:08:49 -0600

    Es la version 2.0 de la calculadora que usa el boton de calcular xd

---
+++ Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿
+namespace Calculadora2._0._1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNumero1 = new System.Windows.Forms.TextBox();
+            this.txtNumero2 = new System.Windows.Forms.TextBox();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.cbOperacion = new System.Windows.Forms.ComboBox();
+            this.btnIgual = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnCleaner = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtNumero1
+            // 
+            this.txtNumero1.Location = new System.Drawing.Point(30, 30);
+            this.txtNumero1.Name = "txtNumero1";
+            this.txtNumero1.Size = new System.Drawing.Size(100, 20);
+            this.txtNumero1.TabIndex = 0;
+            // 
+            // txtNumero2
+            // 
+            this.txtNumero2.Location = new System.Drawing.Point(30, 67);
+            this.txtNumero2.Name = "txtNumero2";
+            this.txtNumero2.Size = new System.Drawing.Size(100, 20);
+            this.txtNumero2.TabIndex = 1;
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Location = new System.Drawing.Point(192, 74);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(0, 13);
+            this.lblResultado.TabIndex = 4;
+            // 
+            // cbOperacion
+            // 
+            this.cbOperacion.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbOperacion.FormattingEnabled = true;
+            this.cbOperacion.Items.AddRange(new object[] {
+            "+",
+            "-",
+            "*",
+            "/"});
+            this.cbOperacion.Location = new System.Drawing.Point(136, 30);
+            this.cbOperacion.Name = "cbOperacion";
+            this.cbOperacion.Size = new System.Drawing.Size(50, 21);
+            this.cbOperacion.TabIndex = 2;
+            // 
+            // btnIgual
+            // 
+            this.btnIgual.Location = new System.Drawing.Point(136, 67);
+            this.btnIgual.Name = "btnIgual";
+            this.btnIgual.Size = new System.Drawing.Size(50, 23);
+            this.btnIgual.TabIndex = 3;
+            this.btnIgual.Text = "=";
+            this.btnIgual.UseVisualStyleBackColor = true;
+            this.btnIgual.Click += new System.EventHandler(this.btnIgual_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(86, 96);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(50, 23);
+            this.btnExit.TabIndex = 5;
+            this.btnExit.Text = "Salir";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnCleaner
+            // 
+            this.btnCleaner.Location = new System.Drawing.Point(30, 96);
+            this.btnCleaner.Name = "btnCleaner";
+            this.btnCleaner.Size = new System.Drawing.Size(50, 23);
+            this.btnCleaner.TabIndex = 6;
+            this.btnCleaner.Text = "Limpiar";
+            this.btnCleaner.UseVisualStyleBackColor = true;
+            this.btnCleaner.Click += new System.EventHandler(this.btnCleaner_Click);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(420, 200);
+            this.Controls.Add(this.btnCleaner);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnIgual);
+            this.Controls.Add(this.cbOperacion);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.txtNumero2);
+            this.Controls.Add(this.txtNumero1);
+            this.Name = "Form1";
+            this.Text = "Calculadora";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private System.Windows.Forms.TextBox txtNumero1;
+        private System.Windows.Forms.TextBox txtNumero2;
+        private System.Windows.Forms.ComboBox cbOperacion;
+        private System.Windows.Forms.Button btnIgual;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnCleaner;
+    }
+
+        #endregion
+}
+
+
---
+++ Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculadora2._0._1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnIgual_Click(object sender, EventArgs e)
+        {
+            double numero1 = Convert.ToDouble(txtNumero1.Text);
+            double numero2 = Convert.ToDouble(txtNumero2.Text);
+            double resultado = 0;
+
+            switch (cbOperacion.SelectedItem.ToString())
+            {
+                case "+":
+                    resultado = numero1 + numero2;
+                    break;
+                case "-":
+                    resultado = numero1 - numero2;
+                    break;
+                case "x":
+                    resultado = numero1 * numero2;
+                    break;
+                case "÷":
+                    if (numero2 != 0)
+                    {
+                        resultado = numero1 / numero2;
+                    }
+                    else
+                    {
+                        lblResultado.Text = "No se puede dividir por 0";
+                        return;
+                    }
+                    break;
+                default:
+                    lblResultado.Text = "Selecciona una operación";
+                    return;
+            }
+
+            lblResultado.Text = "Resultado: " + resultado.ToString();
+
+        }
+
+        private void btnCleaner_Click(object sender, EventArgs e)
+        {
+            txtNumero1.Text = "";
+            txtNumero2.Text = "";
+            lblResultado.Text = "";
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
