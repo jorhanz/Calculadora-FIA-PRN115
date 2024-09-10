42ffb4a12dd15d6e496f855e469a1d7f9fa3987f
Irving-Lopez <LM24022@ti.ues.edu.sv>
2024-09-10 16:02:39 -0600

    cambios LM24022

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -40,73 +40,88 @@ namespace Calculadora2._0._1
             // 
             // txtNumero1
             // 
-            this.txtNumero1.Location = new System.Drawing.Point(30, 30);
+            this.txtNumero1.BackColor = System.Drawing.Color.Aqua;
+            this.txtNumero1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNumero1.Location = new System.Drawing.Point(54, 12);
             this.txtNumero1.Name = "txtNumero1";
-            this.txtNumero1.Size = new System.Drawing.Size(100, 20);
+            this.txtNumero1.Size = new System.Drawing.Size(123, 35);
             this.txtNumero1.TabIndex = 0;
             // 
             // txtNumero2
             // 
-            this.txtNumero2.Location = new System.Drawing.Point(30, 67);
+            this.txtNumero2.BackColor = System.Drawing.Color.Aqua;
+            this.txtNumero2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNumero2.Location = new System.Drawing.Point(54, 53);
             this.txtNumero2.Name = "txtNumero2";
-            this.txtNumero2.Size = new System.Drawing.Size(100, 20);
+            this.txtNumero2.Size = new System.Drawing.Size(123, 35);
             this.txtNumero2.TabIndex = 1;
             // 
             // lblResultado
             // 
             this.lblResultado.AutoSize = true;
-            this.lblResultado.Location = new System.Drawing.Point(192, 74);
+            this.lblResultado.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.lblResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResultado.Location = new System.Drawing.Point(33, 96);
             this.lblResultado.Name = "lblResultado";
-            this.lblResultado.Size = new System.Drawing.Size(0, 13);
+            this.lblResultado.Size = new System.Drawing.Size(0, 29);
             this.lblResultado.TabIndex = 4;
             // 
             // cbOperacion
             // 
+            this.cbOperacion.BackColor = System.Drawing.Color.Aqua;
             this.cbOperacion.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbOperacion.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.cbOperacion.FormattingEnabled = true;
             this.cbOperacion.Items.AddRange(new object[] {
             "+",
             "-",
             "*",
             "/"});
-            this.cbOperacion.Location = new System.Drawing.Point(136, 30);
+            this.cbOperacion.Location = new System.Drawing.Point(184, 10);
             this.cbOperacion.Name = "cbOperacion";
-            this.cbOperacion.Size = new System.Drawing.Size(50, 21);
+            this.cbOperacion.Size = new System.Drawing.Size(46, 37);
             this.cbOperacion.TabIndex = 2;
             // 
             // btnIgual
             // 
-            this.btnIgual.Location = new System.Drawing.Point(136, 67);
+            this.btnIgual.BackColor = System.Drawing.Color.Aqua;
+            this.btnIgual.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnIgual.Location = new System.Drawing.Point(184, 53);
             this.btnIgual.Name = "btnIgual";
-            this.btnIgual.Size = new System.Drawing.Size(50, 23);
+            this.btnIgual.Size = new System.Drawing.Size(46, 35);
             this.btnIgual.TabIndex = 3;
             this.btnIgual.Text = "=";
-            this.btnIgual.UseVisualStyleBackColor = true;
+            this.btnIgual.UseVisualStyleBackColor = false;
             this.btnIgual.Click += new System.EventHandler(this.btnIgual_Click);
             // 
             // btnExit
             // 
-            this.btnExit.Location = new System.Drawing.Point(86, 96);
+            this.btnExit.BackColor = System.Drawing.Color.Aqua;
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(142, 133);
             this.btnExit.Name = "btnExit";
-            this.btnExit.Size = new System.Drawing.Size(50, 23);
+            this.btnExit.Size = new System.Drawing.Size(120, 38);
             this.btnExit.TabIndex = 5;
             this.btnExit.Text = "Salir";
-            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.UseVisualStyleBackColor = false;
             this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
             // 
             // btnCleaner
             // 
-            this.btnCleaner.Location = new System.Drawing.Point(30, 96);
+            this.btnCleaner.BackColor = System.Drawing.Color.Aqua;
+            this.btnCleaner.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCleaner.Location = new System.Drawing.Point(12, 133);
             this.btnCleaner.Name = "btnCleaner";
-            this.btnCleaner.Size = new System.Drawing.Size(50, 23);
+            this.btnCleaner.Size = new System.Drawing.Size(120, 38);
             this.btnCleaner.TabIndex = 6;
             this.btnCleaner.Text = "Limpiar";
-            this.btnCleaner.UseVisualStyleBackColor = true;
+            this.btnCleaner.UseVisualStyleBackColor = false;
             this.btnCleaner.Click += new System.EventHandler(this.btnCleaner_Click);
             // 
             // Form1
             // 
-            this.ClientSize = new System.Drawing.Size(420, 200);
+            this.BackColor = System.Drawing.Color.Teal;
+            this.ClientSize = new System.Drawing.Size(280, 200);
             this.Controls.Add(this.btnCleaner);
             this.Controls.Add(this.btnExit);
             this.Controls.Add(this.btnIgual);
@@ -114,7 +129,9 @@ namespace Calculadora2._0._1
             this.Controls.Add(this.lblResultado);
             this.Controls.Add(this.txtNumero2);
             this.Controls.Add(this.txtNumero1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Calculadora";
             this.ResumeLayout(false);
             this.PerformLayout();
--- Form1.cs
+++ Form1.cs
@@ -1,28 +1,51 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Globalization;
 using System.Windows.Forms;
 
 namespace Calculadora2._0._1
 {
     public partial class Form1 : Form
     {
+        // Constructor del formulario
         public Form1()
         {
             InitializeComponent();
+            KeyPreview = true; // Habilita la captura de teclas para el formulario
+            this.KeyPress += new KeyPressEventHandler(Form1_KeyPress); // Asocia el manejador de eventos para la tecla presionada
         }
 
+        // Manejador del clic del botón "="
         private void btnIgual_Click(object sender, EventArgs e)
         {
-            double numero1 = Convert.ToDouble(txtNumero1.Text);
-            double numero2 = Convert.ToDouble(txtNumero2.Text);
-            double resultado = 0;
+            // Validar que ambos campos de entrada no estén vacíos
+            if (string.IsNullOrEmpty(txtNumero1.Text) || string.IsNullOrEmpty(txtNumero2.Text))
+            {
+                lblResultado.Text = "Por favor, ingresa ambos números.";
+                return;
+            }
 
+            double numero1, numero2, resultado = 0;
+
+            // Intentar convertir las entradas a números de acuerdo a la configuración regional del sistema
+            try
+            {
+                numero1 = double.Parse(txtNumero1.Text, CultureInfo.CurrentCulture);
+                numero2 = double.Parse(txtNumero2.Text, CultureInfo.CurrentCulture);
+            }
+            catch (FormatException)
+            {
+                lblResultado.Text = "Por favor, ingresa números válidos.";
+                return;
+            }
+
+            // Validar que se haya seleccionado una operación
+            if (cbOperacion.SelectedItem == null)
+            {
+                lblResultado.Text = "Selecciona una operación.";
+                return;
+            }
+
+            // Realizar el cálculo basado en la operación seleccionada
             switch (cbOperacion.SelectedItem.ToString())
             {
                 case "+":
@@ -41,29 +64,68 @@ namespace Calculadora2._0._1
                     }
                     else
                     {
-                        lblResultado.Text = "No se puede dividir por 0";
+                        lblResultado.Text = "No se puede dividir por 0.";
                         return;
                     }
                     break;
                 default:
-                    lblResultado.Text = "Selecciona una operación";
+                    lblResultado.Text = "Operación no válida.";
                     return;
             }
 
-            lblResultado.Text = "Resultado: " + resultado.ToString();
+            // Mostrar el resultado en el label con el formato adecuado
+            lblResultado.Text = "Resultado: " + resultado.ToString("G", CultureInfo.CurrentCulture);
 
+            // Guardar la operación en el historial
+            GuardarHistorial(numero1, numero2, cbOperacion.SelectedItem.ToString(), resultado);
+
+            // Preparar los campos para la siguiente operación
+            PrepararSiguienteOperacion(resultado);
         }
 
+        // Método para limpiar los campos de entrada y el resultado
+        private void LimpiarCampos()
+        {
+            txtNumero1.Clear(); // Limpia el campo de texto del primer número
+            txtNumero2.Clear(); // Limpia el campo de texto del segundo número
+            lblResultado.Text = string.Empty; // Limpia el label del resultado
+            cbOperacion.SelectedIndex = -1; // Deselecciona la operación
+        }
+
+        // Método para preparar los campos para la siguiente operación
+        private void PrepararSiguienteOperacion(double resultado)
+        {
+            txtNumero1.Text = resultado.ToString(CultureInfo.CurrentCulture); // Usa el resultado como primer número para la siguiente operación
+            txtNumero2.Clear(); // Limpia el campo del segundo número
+            cbOperacion.SelectedIndex = -1; // Deselecciona la operación
+        }
+
+        // Método para guardar la operación en el historial
+        private void GuardarHistorial(double numero1, double numero2, string operacion, double resultado)
+        {
+            // Agrega la operación al ListBox del historial
+            listBoxHistorial.Items.Add($"{numero1.ToString(CultureInfo.CurrentCulture)} {operacion} {numero2.ToString(CultureInfo.CurrentCulture)} = {resultado.ToString(CultureInfo.CurrentCulture)}");
+        }
+
+        // Manejador del clic del botón "Limpiar"
         private void btnCleaner_Click(object sender, EventArgs e)
         {
-            txtNumero1.Text = "";
-            txtNumero2.Text = "";
-            lblResultado.Text = "";
+            LimpiarCampos(); // Llama al método para limpiar los campos
         }
 
+        // Manejador del clic del botón "Salir"
         private void btnExit_Click(object sender, EventArgs e)
         {
-            Application.Exit();
+            Application.Exit(); // Cierra la aplicación
+        }
+
+        // Manejador del evento de presionar teclas
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Enter)
+            {
+                btnIgual.PerformClick(); // Ejecuta la operación cuando se presiona Enter
+            }
         }
     }
 }
